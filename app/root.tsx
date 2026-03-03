6d67021ebcd5038aa6f5ad98814c7e81b3db88a4
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2026-03-03 15:33:07 +0100

    fix: remove leftover container

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type {LinksFunction} from "@remix-run/cloudflare";
+import type { LinksFunction } from "@remix-run/cloudflare";
 import {
     Links,
     Meta,
@@ -10,7 +10,7 @@ import {
 import "./tailwind.css";
 
 export const links: LinksFunction = () => [
-    {rel: "preconnect", href: "https://fonts.googleapis.com"},
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
     {
         rel: "preconnect",
         href: "https://fonts.gstatic.com",
@@ -22,38 +22,26 @@ export const links: LinksFunction = () => [
     },
 ];
 
-export function Layout({children}: { children: React.ReactNode }) {
+export function Layout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <head>
-            <meta charSet="utf-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <Meta/>
-            <Links/>
-        </head>
-        <body>
-        {children}
-        <ScrollRestoration/>
-        <Scripts/>
-        </body>
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                {children}
+                <ScrollRestoration />
+                <Scripts />
+            </body>
         </html>
     );
 }
 
 export default function App() {
     return (
-        <div className="relative m-4 h-screen">
-            {/* Title */}
-            <div className="absolute -top-3 left-1/2 -translate-x-1/2 px-4 bg-black text-white">
-            <span className="text-sm border border-white px-2 py-1">
-              my-portfolio.1
-            </span>
-            </div>
-
-            {/* Bordered container */}
-            <div className="border border-white p-8">
-                <Outlet/>
-            </div>
-        </div>
+        <Outlet />
     );
 }
