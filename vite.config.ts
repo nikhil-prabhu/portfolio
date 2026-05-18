4b0a98e54d58ea082a2a36eeb580a58686d665f3
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2026-05-18 18:09:46 +0530

    dev: add lan dev devices to `allowedHosts`

--- vite.config.ts
+++ vite.config.ts
@@ -28,5 +28,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: [
+      "alduin.local",
+      "paarthurnax.local",
+      "sahrotaar.local",
+      "naaslaarum.local",
+      "voslaarum.local",
+    ],
   },
 });
